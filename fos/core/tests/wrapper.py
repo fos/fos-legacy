8076215f38700a2e2b14f4651bd70ae935076586
Ian Nimmo-Smith <ian.nimmo-smith@mrc-cbu.cam.ac.uk>
2010-04-17 12:43:06 +0100

    wrapper tweaking

--- fos/core/tests/wrapper.py
+++ fos/core/tests/wrapper.py
@@ -9,26 +9,29 @@ wrapper_original = r'''
 \usepackage{helvet}
 \usepackage{courier}
 \usepackage{textcomp}
-\usepackage[papersize={210mm,297mm},body={190mm,277in},margin={10mm,10mm}]{geometry}
+%\usepackage[papersize={210mm,297mm},body={190mm,277in},margin={10mm,10mm}]{geometry}
 \usepackage {graphicx}
 \pagestyle{empty}
+\usepackage[none]{hyphenat}
 \begin{document}
+\raggedright
 \thispagestyle{empty}
 '''
 
-wrapper_new = r'''
-\documentclass{article} 
-\usepackage{amsmath}
-\usepackage{amsthm}
-\usepackage{amssymb}
-\usepackage{bm}
-\usepackage{fancybox}
-\newcommand{\mx}[1]{\mathbf{\bm{#1}}} 
-\newcommand{\vc}[1]{\mathbf{\bm{#1}}} 
-\newcommand{\T}{\text{T}}             
-\pagestyle{empty} 
-\begin{document} 
-'''
+#wrapper_new = r'''
+#\documentclass{article} 
+#\usepackage{amsmath}
+#\usepackage{amsthm}
+#\usepackage{amssymb}
+#\usepackage{bm}
+#%\makeatletter
+#%\usepackage{fancybox}
+#\newcommand{\mx}[1]{\mathbf{\bm{#1}}} 
+#\newcommand{\vc}[1]{\mathbf{\bm{#1}}} 
+#\newcommand{\T}{\text{T}}             
+#\pagestyle{empty} 
+#\begin{document} 
+#'''
 
 closing = r'''
 \end{document}
@@ -41,15 +44,18 @@ closing = r'''
 #'''
 
 snippet = r'''
-%\fontsize{16}{20}
-%\resizebox{120mm}{!}{%
-%\shadowbox{\parbox{60cm}{%
+\fontsize{24}{30}
 \sffamily
-  \begin{itemize}
-    \item[\texttt{bundle}] spino-cortical tract
-    \item[\texttt{\#tracks}] 100,000
-    \item[\texttt{status}] provisional
-  \end{itemize}%}%}%}}
+\resizebox{120mm}{!}{%
+  \begin{minipage}{90mm}
+    \raggedright
+    \begin{itemize}
+      \item[\texttt{bundle}] spino-cortical tract or some such structure
+      \item[\texttt{\#tracks}] 100,000+ or so
+      \item[\texttt{status}] provisional till \textsc{LARCH} gets to work
+    \end{itemize}
+  \end{minipage}
+}% end of resizebox
 '''
 
 document = wrapper_original + snippet + closing
@@ -72,36 +78,32 @@ latexfilehandle.close()
 
 # compile LaTeX document. A DVI file is created
 
-latex_process = subprocess.Popen(['latex -interaction=nonstopmode %s'  % latexfile], shell=True, cwd=os.getcwd())
+latex_process = subprocess.Popen(['latex -interaction=nonstopmode '
+                                  + latexfile], shell=True, stdout=open('/dev/null'))
 
 latex_process.wait()
 
 # Run dvipng on the generated DVI file. Use tight bounding box. 
 # Magnification is set to 1200
 
-#cmd_original = ["dvipng -o", "data/"+pngfile, "data/"+dvifile]
-
 cmd_original = ["dvipng -o " + pngfile + ' ' + label]
-
-print os.getcwd()
 
 cmd_new = ["dvipng -q -T tight -x 1000 -z 1 -bg 'rgb 1 0.6 0.6' -o" + pngfile + ' ' + label]
 
-print cmd_new
-
-dvipng_process = subprocess.Popen(cmd_new, shell=True)
+dvipng_process = subprocess.Popen(cmd_new, shell=True, stdout=open('/dev/null'))
 
 dvipng_process.wait()
 
-eog_process = subprocess.Popen(['eog ' + pngfile], shell=True)
+eog_process = subprocess.Popen(['eog ' + pngfile], shell=True, stdout=open('/dev/null'))
 
 eog_process.wait()
 
 # Remove temporary files
 os.remove(label+'.tex')
-os.remove(label+'.log')
+#os.remove(label+'.log')
 os.remove(label+'.aux')
 os.remove(label+'.dvi')
+#os.remove('png/'+label+'.png')
 
 # but don't do it too soon!
 
