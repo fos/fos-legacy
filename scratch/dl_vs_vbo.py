b50444beede3ea73531245462c390f64c960a281
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2010-06-29 23:34:18 +0100

    NF points with Display lists and VBOs

--- scratch/dl_vs_vbo.py
+++ scratch/dl_vs_vbo.py
@@ -3,16 +3,22 @@ from fos.core.scene  import Scene
 from fos.core.actors import Actor
 from fos.core.plots  import Plot
 from fos.core.tracks import Tracks
-from fos.core.tracks import Points
+from fos.core.points import Points
 
 
-data=200*np.random.rand(1000,3)
-colors=np.random.rand(1000,4)
+data=200*np.random.rand(1000000,3)
+colors=np.random.rand(1000000,4)
 
-#tr=Tracks(data,colors)
+print('no of bytes',colors.nbytes + data.nbytes)
+
+tr=Tracks([data],[colors],lists=True)
 #slot={0:{'actor':tr,'slot':(0, 800000)}}
 #Scene(Plot(slot)).run()
 
-pts=Points([data],[colors])
-slot={0:{'actor':pts,'slot':(0, 800000)}}
+pts=Points([data],[colors],point_size=3.,lists=True)
+
+slot={0:{'actor':tr,'slot':(0, 800000)},
+      1:{'actor':pts,'slot':(0, 800000)}}
 Scene(Plot(slot)).run()
+
+
