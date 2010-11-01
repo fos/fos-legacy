efff8240c130e4feee7bc107611a67c9b4437ba7
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2010-11-01 17:29:22 +0000

    RF -color.py added

---
+++ fos/core/color.py
@@ -0,0 +1,15 @@
+from fos.lib.pyglet.gl import *
+
+red = (1., 0, 0, 1.)
+orange = (1., 0.498, 0, 1)
+yellow = (1, 1, 0, 1.)
+green = (0, 1, 0, 1.)
+cyan = (0, 1., 1., 1.)
+blue = (0, 0, 1., 1.)
+purple = (1., 0, 1., 1.)
+white = (1.,1,1,1)
+grey = (0.498, 0.498, 0.498, 1.)
+black = (0, 0, 0, 1.)
+
+
+
