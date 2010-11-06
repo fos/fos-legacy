e3e78154ef8fd98d9a158d8cf0f8c930e2e25bc1
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2010-11-06 10:22:19 +0000

    test for curves

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Fos'
-copyright = u'2010, Eleftherios Garyfallidis & Stephan Gerhard'
+copyright = u'2010, Eleftherios Garyfallidis, Stephan Gerhard'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
