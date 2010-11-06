ddcf0daa40edf0f2c819e970c9eb902140bd8e34
Stephan Gerhard <git@unidesign.ch>
2010-11-06 10:49:52 +0000

    Add new example

---
+++ doc/faq.rst
@@ -0,0 +1,11 @@
+FAQ
+===
+
+* Is it possible to have an actor for my scientific visualization needs?
+
+If the actors are not sufficient for what you need to do, you can write your own actor
+that fulfills your needs. We are happy to help you with that.
+
+* I want to write an email to the main developers of Fos?
+
+We are open for questions and suggestions. Write an email to fos@unidesign.ch !
---
+++ examples/multimodal.py
@@ -0,0 +1,24 @@
+import numpy as np
+import nibabel as nib
+
+from fos import World, Window
+from fos.actor.curve import InteractiveCurves
+
+
+#w = wi.get_world()
+
+# file
+fname = '/home/stephan/Downloads/Tracks/fibers.trk'
+streams,hdr = nib.trackvis.read(fname)
+
+T=[s[0] for s in streams]
+Trk=np.array(T, dtype=np.object)
+print "loaded file"
+
+cu = InteractiveCurves(curves = T, centered = False)
+
+w=World()
+w.add(cu)
+
+wi = Window(caption="Multi-Modal 1")
+wi.attach(w)
