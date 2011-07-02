75d43a25dcdf0b1b06b8f439783a41d20a737367
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2011-07-02 13:08:25 -0400

    surface example corrected

--- examples/surface_simple.py
+++ examples/surface_simple.py
@@ -1,5 +1,8 @@
 import numpy as np
 from fos.comp_geom.gen_normals import auto_normals 
+from fos.data import get_sphere
+from fos.actor.surf import Surface
+from fos import Window, WindowManager, World, DefaultCamera
 
 #import os.path as op
 #pa = '/home/stephan/Downloads/Gifti/'
@@ -25,45 +28,37 @@ colors=colors.astype(np.float32)
 print colors.shape, colors.min(),colors.max(),colors.mean()
 """
 
-eds=np.load('/home/eg309/Devel/dipy/dipy/core/matrices/evenly_distributed_sphere_362.npz')
+
+eds=np.load(get_sphere())
 vertices=eds['vertices']
 faces=eds['faces']
 
-vertices=vertices.astype(np.float32)
+vertices=100*vertices.astype(np.float32)
 faces=faces.astype(np.uint32)        
 
-len(vertices)
 print 'vertices.shape', vertices.shape, vertices.dtype
 print 'faces.shape', faces.shape,faces.dtype
-    
+
+colors=np.random.rand(len(vertices),4)
+colors=colors.astype('f4')
+   
 normals=auto_normals(vertices,faces)
-print 'normals.shape',normals.shape
-print 'colors.shape',colors.shape
+print('normals.shape %d %d' % normals.shape)
 
-print vertices.min(),vertices.max(),vertices.mean()
-print normals.min(),normals.max(), normals.mean()
+print('colors.shape %d %d' % colors.shape)
+print(vertices.dtype,faces.dtype, colors.dtype, normals.dtype)
 
-print vertices.dtype,faces.dtype, colors.dtype, normals.dtype
-
-from fos.actor.surf import Surface
 aff = np.eye(4, dtype = np.float32)
 aff[:3,3] = [150,0,0]
 
 s=Surface(vertices,faces,colors, affine = aff, add_lights = True, normals = normals)
 
-#vertices2=vertices+np.array([100,0,0],dtype=vertices.dtype)
-#s2=Surface(vertices2,faces,normals,colors)
-
-from fos.core.world import World
 w=World()
 w.add(s)
-#w.add(s2)
 
-from fos.core.camera import DefaultCamera
 cam=DefaultCamera()
 w.add(cam)
-
-from fos import Window, WindowManager 
+ 
 wi=Window()
 wi.attach(w)
 
